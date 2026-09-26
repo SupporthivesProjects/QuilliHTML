8c9ab0ddd298dbe91320d5d290b77f1e05b079a8
vijay <vijay@supporthive.com>
2026-09-26 14:23:40 +0530

    error page

--- error.php
+++ error.php
@@ -1,5 +1,24 @@
-
 <?php include 'includes/header.php'; ?>
-
-
+<section class="error-top">
+    <div class="container">
+        <div class="col">
+            <div class="row">
+                <div class="col-lg-6 col-md-12 col-12 col-sm-12 align-content-center align-self-center order-1 order-lg-0">
+                    <div class="error-contanet">
+                        <h6>404 · WE HAVE LOST THE THREAD.</h6>
+                        <h1>Page not found</h1>
+                        <p>
+                            That page has moved, or the link has taken a 
+                            wrong turn. Let us get you back to the story.
+                        </p>
+                        <a href="#" class="btn btn-black"> Back to homepage</a>
+                    </div>
+                </div>
+                <div class="col-lg-6 col-md-12 col-12 col-sm-12">
+                    <img src="./img/404.png" alt="" class="img-fluid" srcset="./img/404.png">
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
 <?php include 'includes/footer.php'; ?>
